23c28e17d30b820c892f4d8a77d41686a7d975ad
龚俊豪 <853788283@qq.com>
2021-05-21 16:40:41 +0800

    second

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,62 @@
 // pages/category/index.js
+import {
+  request
+} from "../../request/index.js";
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    //左侧菜单数据
+    leftMenuList: [],
+    //右侧商品数据
+    rightContent: [],
+    //被点击的左侧的菜单
+    currentIndex: 0
   },
+  //接口的返回数据
+  Cates: [],
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getCates();
   },
+  //获取分类数据
+  getCates() {
+    request({
+        url: "https://api-hmugo-web.itheima.net/api/public/v1/categories"
+      })
+      .then(res => {
+        this.Cates = res.data.message;
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+        //构造左侧大菜单数据
+        let leftMenuList = this.Cates.map(v => v.cat_name);
+        //构造右侧的商品数据
+        let rightContent = this.Cates[0].children;
+        this.setData({
+          leftMenuList,
+          rightContent
+        })
+      })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //左侧菜单的点击事件
+  handleItemTap(e) {
+    // 1.取被点击的标题身上的索引
+    // 2.给data中的currentIndex赋值
+    const {
+      index
+    } = e.currentTarget.dataset;
+    this.setData({
+      currentIndex: index,
+      rightContent
+    })
+    let rightContent = this.Cates[index].children;
+    this.setData({
+      currentIndex: index,
+      rightContent
+    })
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,21 +1,70 @@
-//Page Object
+//Page Object 0 引入用来发送请求 的方法 一定补全路径
+import {
+    request
+} from "../../request/index.js";
 Page({
     data: {
-        //轮播图数组
-        swiperList: []
+        //轮播图 数组
+        swiperList: [],
+        //导航 数组
+        catesList: [],
+        floorList: [],
     },
     //页面开始加载就会触发
     //options(Object)
     onLoad: function (options) {
-        // 1 发送异步请求获取轮播图数据
-        wx.request({
-            url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
-            success: (result) => {
+        // // 1 发送异步请求获取轮播图数据
+        // wx.request({
+        //     url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+        //     success: (result) => {
+        //         this.setData({
+        //             swiperList: result.data.message
+        //         })
+        //     }
+        // });
+        this.getSwiperList();
+        this.getCateList();
+        this.getFloorList();
+    },
+
+    //获取轮播图数据
+    getSwiperList() {
+        request({
+            url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata"
+        }).then(
+            result => {
                 this.setData({
                     swiperList: result.data.message
                 })
             }
-        });
-
+        )
     },
+
+    //获取轮播图数据
+    getCateList() {
+        request({
+            url: "https://api-hmugo-web.itheima.net/api/public/v1/home/catitems"
+        }).then(
+            result => {
+                this.setData({
+                    catesList: result.data.message
+                })
+            }
+        )
+    },
+
+    //获取楼层图数据
+    getFloorList() {
+        request({
+            url: "https://api-hmugo-web.itheima.net/api/public/v1/home/floordata"
+        }).then(
+            result => {
+                this.setData({
+                    floorList: result.data.message
+                })
+            }
+        )
+    },
+
+
 });
